042417f859adadcbd43366c1c54370612774f92c
hemantkishore7 <hemantkishore98@gmail.com>
2023-06-07 17:20:03 +0530

    link update

--- routes/passwordReset.route.js
+++ routes/passwordReset.route.js
@@ -37,7 +37,7 @@ passwordResetRoute.post("/password-reset", async (req, res) => {
       }).save();
     }
 
-    const url = `https://localhost:3000/reset/${user._id}/${token.token}/`;
+    const url = `http://localhost:3000/reset/${user._id}/${token.token}/`;
     // http://localhost:3000/reset
     await sendMail(user.email, "Password Reset", url);
     res.status(200).send({
